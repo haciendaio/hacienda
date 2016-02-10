02b900bf7c76b77d9c3a05a2c0d69cfb3717e3a0
Karl Stoney <kstoney@thoughtworks.com>
2016-02-10 15:43:01 +0000

    Karl Have rugged wrapper automatically close the repo

--- app/utilities/rugged_wrapper.rb
+++ app/utilities/rugged_wrapper.rb
@@ -19,7 +19,9 @@ module Hacienda
     end
 
     def get_repo
-      Repository.new(@repo_path)
+      repo = Repository.new(@repo_path)
+      yield(repo)
+      repo.close
     end
 
     def get_wrapper_for_repo repo
@@ -27,32 +29,36 @@ module Hacienda
     end
 
     def sha_for(item_path)
-      get_repo.head.target.tree.path(item_path)[:oid]
+      get_repo do |repo| 
+        repo.head.target.tree.path(item_path)[:oid]
+      end
     end
 
-    def get_version_in_past(file_path, changes_in_the_past, repo = get_repo, walker = get_wrapper_for_repo(repo))
+    def get_version_in_past(file_path, changes_in_the_past)
       raise ArgumentError if changes_in_the_past < 0
-      walker.push(repo.last_commit)
 
-      last_blob = repo.blob_at(repo.last_commit.oid, file_path)
-      current_blob_id = last_blob.oid
+      get_repo do |repo|  
+        walker = get_wrapper_for_repo(repo))
+        walker.push(repo.last_commit)
 
-      walker.each do |commit|
-        blob = repo.blob_at(commit.oid, file_path)
-        blob_id = (blob ? blob.oid : 0)
+        last_blob = repo.blob_at(repo.last_commit.oid, file_path)
+        current_blob_id = last_blob.oid
 
-        if current_blob_id != blob_id
-          current_blob_id = blob_id
-          last_blob = blob
-          changes_in_the_past -= 1
+        walker.each do |commit|
+          blob = repo.blob_at(commit.oid, file_path)
+          blob_id = (blob ? blob.oid : 0)
+
+          if current_blob_id != blob_id
+            current_blob_id = blob_id
+            last_blob = blob
+            changes_in_the_past -= 1
+          end
+          break if (changes_in_the_past == 0)
+          break unless last_blob
         end
-        break if (changes_in_the_past == 0)
-        break unless last_blob
       end
-
       changes_in_the_past == 0 ? last_blob : nil
     end
-
   end
 
 end
